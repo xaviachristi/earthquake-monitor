7768fc9a1cf71ca363e2b419dfc33ac016cd5cdc
Em <trainee.emily.prust@sigmalabs.co.uk>
2025-06-10 18:10:46 +0100

    Experiment with creating a dataframe

--- pipeline/README.md
+++ pipeline/README.md
@@ -15,7 +15,7 @@
 
 - ObsPlus wanted a version on python with `tkinter`. I could do this now but it seems like it could cause more issues down the line.
 
-- I can still use ObsPy to get the data (I've considered the alternative of removing this library and calling the API directly, however it seems using obspy will make it easier to expand the project to different data sources in the future).
+- I can still use ObsPy to get the data (I've considered the alternative of removing this library and calling the API directly, however it seems using obspy will make it easier to expand the project to different data sources in the future). [Helpful docs for this stage.](https://docs.obspy.org/tutorial/code_snippets/retrieving_data_from_datacenters.html#the-fdsn-web-services)
 
 - Now the big question is how to get the `ObsPy.Catalog` object into a `pd.DataFrame`.
 
@@ -32,7 +32,7 @@
     - `obspy.io.json()` --`JSON`-->
     - `pd.read_json()` --`pd.DataFrame`--> `transform.py`.
 
-
+- Converting the ObsPy catalog to JSON is fine, but getting that JSON into a dataframe is difficult, as the data has a complex structure.
 
 
 Is this missing any data we need?
--- pipeline/extract.py
+++ pipeline/extract.py
@@ -4,11 +4,13 @@ into a Pandas DataFrame.
 """
 
 import logging
+import json
 from datetime import datetime
 
 import pandas as pd
 from obspy import Catalog
 import obspy.clients.fdsn
+import obspy.io.json
 
 
 logger = logging.getLogger(__name__)
@@ -26,7 +28,7 @@ def access_api(api_base_url: str,) -> Catalog:
     logger.info("Connecting to the API: %s", api_base_url)
     client = obspy.clients.fdsn.Client(api_base_url)
 
-    logger.debug(client)
+    logger.debug("API client:\n%s",client)
 
     logger.info("Retrieving events from API: %s", api_base_url)
     return client.get_events()
@@ -34,9 +36,44 @@ def access_api(api_base_url: str,) -> Catalog:
 
 def convert_catalog_to_dataframe(catalog: Catalog) -> pd.DataFrame:
     """Converts an ObsPy catalog to a DataFrame."""
-    pass
+
+    logger.info("Converting catalog to dataframe.")
+
+    catalog.write("test_write.json", format="JSON")
 
 
 if __name__ == "__main__":
 
-    print(access_api("USGS"))
+    # res = access_api("USGS")
+    # print(convert_catalog_to_dataframe(res))
+
+    # data = pd.read_json("test_write.json")
+    with open("test_write.json", encoding="utf-8") as f:
+        data = json.load(f)
+        df = pd.json_normalize(data["events"], max_level=3)
+        print(df)
+        print(df.info())
+
+
+        # Checklist for this data (this checklist may be outdated, depending on the ERD):
+
+        # - magnitude
+        print(df["magnitudes"]) # A dictionary containing magnitude information.
+        # - time
+        # - latitude
+        # - longitude
+        # - depth
+        # - url
+        print(df["origins"]) # A list of dictionaries containing:
+                # URL & time;
+                # Longitude;
+                # Latitude;
+                # Depth;
+                # and others which are mostly irrelevant and include a lot of nulls.
+        # - updated
+        # - tsunami
+        # - type
+        print(df["event_type"]) # From what I can see, mostly "earthquake"!
+
+    # Extract these here or in transform?
+    # I would assume Transform is expecting a cleaner dataframe?
