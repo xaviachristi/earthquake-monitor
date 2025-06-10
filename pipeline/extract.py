6e44b05c63f9c4189db7c096bbb0d702399af781
Em <trainee.emily.prust@sigmalabs.co.uk>
2025-06-10 15:00:25 +0100

    Create basic files and function signatures

---
+++ pipeline/extract.py
@@ -0,0 +1,29 @@
+"""
+Connects to the USGS earthquake API and converts the information
+into a Pandas DataFrame.
+"""
+
+import logging
+
+import pandas as pd
+import obspy
+import obsplus
+
+
+logger = logging.getLogger(__name__)
+
+logging.basicConfig(
+    level="WARNING",
+    format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+    datefmt="%Y-%m-%dT%H:%M:%S"
+)
+
+
+def access_api(url: str) -> obspy.Catalog:
+    """Calls the API, returning its data in a Catalog."""
+    pass
+
+
+def convert_catalog_to_dataframe(catalog: obspy.Catalog) -> pd.DataFrame:
+    """Converts an ObsPy catalog to a DataFrame."""
+    pass
---
+++ pipeline/test_extract.py
@@ -0,0 +1,17 @@
+"""Unit tests for the functions in extract.py."""
+
+from extract import convert_catalog_to_dataframe
+
+import obspy
+
+
+class TestConvertCatalogToDataFrame:
+
+    def test_convert_catalog_to_dataframe_returns_dataframe(self):
+        test_cat = obspy._create_example_catalog()
+
+    def test_convert_catalog_to_dataframe_returns_dataframe_with_correct_data(self):
+        pass
+
+    def test_convert_catalog_to_dataframe_returns_dataframe_with_correct_data(self):
+        pass
