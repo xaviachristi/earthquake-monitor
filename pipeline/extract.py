bb41761815a68d08d004471339368db51b791b62
Em <trainee.emily.prust@sigmalabs.co.uk>
2025-06-11 14:27:57 +0100

    Create extract() function, update imports

--- pipeline/extract.py
+++ pipeline/extract.py
@@ -9,7 +9,8 @@ from datetime import datetime, timedelta
 
 import pandas as pd
 from obspy import Catalog
-import obspy.clients.fdsn
+from obspy.clients.fdsn import Client
+from obspy.clients.fdsn.header import FDSNNoDataException
 
 
 logger = logging.getLogger(__name__)
@@ -25,7 +26,7 @@ def access_api(api_base_url: str, start: datetime, end: datetime) -> Catalog:
     """Calls the API, returning its data in a Catalog."""
 
     logger.info("Connecting to the API: %s", api_base_url)
-    client = obspy.clients.fdsn.Client(api_base_url)
+    client = Client(api_base_url)
 
     logger.info("Retrieving events from %s to %s from the %s API.",
                 start, end, api_base_url)
@@ -51,22 +52,16 @@ def read_json_to_dataframe(filename: str) -> pd.DataFrame:
     return df
 
 
-def convert_catalog_to_dataframe(catalog_to_convert: Catalog,
-                                 file_name: str = "temp_earthquake_data.json"
-                                 ) -> pd.DataFrame:
-    """
-    Takes an ObsPy Catalog and converts it into a Pandas DataFrame.
-    Requires it to be written as a temporary JSON file.
-    """
+def extract(api: str, start_time: datetime, end_time: datetime) -> pd.DataFrame:
+    """Calls the specified API and returns a DataFrame of the information."""
 
-    write_catalog_as_json(file_name, catalog_to_convert)
-    earthquake_data = read_json_to_dataframe(file_name)
-    return earthquake_data
+    try:
+        return convert_catalog_to_dataframe(access_api(api, start_time, end_time))
+
+    except FDSNNoDataException:
+        logger.info("No information for the time period.")
+        return False
 
 
 if __name__ == "__main__":
-    try:
-        logger.info(convert_catalog_to_dataframe(
-            access_api("USGS", datetime.now() - timedelta(weeks=2), datetime.now())))
-    except obspy.clients.fdsn.header.FDSNNoDataException:
-        logger.info("No information for the time period.")
+    extract("USGS", datetime.now() - timedelta(weeks=2), datetime.now())
