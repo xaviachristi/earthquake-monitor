98a658182608b809df44386f9ae71872cfdfb810
kcw36 <kylewat2000@hotmail.com>
2025-06-11 15:22:03 +0100

    Provide test file skeletons classes.

--- dashboard/test_data.py
+++ dashboard/test_data.py
@@ -1,2 +1,8 @@
 # pylint: skip-file
 """Tests for data module."""
+
+from subscription import make_subscription
+
+
+class TestMakeSubscription:
+    """Class that groups tests for make_subscription."""
--- dashboard/test_report.py
+++ dashboard/test_report.py
@@ -1,2 +1,12 @@
 # pylint: skip-file
-"""Tests for data module."""
+"""Tests for report module."""
+
+from data import get_counts_by_state, get_data
+
+
+class TestGetCountsByState:
+    """Class that groups tests for get_count_by_state."""
+
+
+class TestGetData:
+    """Class that groups tests for get_data."""
