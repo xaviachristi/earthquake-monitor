e8992412372fb2cabde72ee980a1b111fd19c22d
Em <trainee.emily.prust@sigmalabs.co.uk>
2025-06-11 12:32:43 +0100

    Remove commented out code

--- pipeline/test_extract.py
+++ pipeline/test_extract.py
@@ -8,21 +8,17 @@ import pandas as pd
 class TestConvertCatalogToDataFrame:
 
     def test_convert_catalog_to_dataframe_returns_dataframe(self, example_catalog):
-        # test_cat = obspy.core.event.catalog._create_example_catalog()
         assert isinstance(convert_catalog_to_dataframe(
             example_catalog), pd.DataFrame)
 
     def test_convert_catalog_to_dataframe_has_magnitudes_column(self, example_catalog):
-        # test_cat = obspy.core.event.catalog._create_example_catalog()
         assert "magnitudes" in convert_catalog_to_dataframe(
             example_catalog).columns
 
     def test_convert_catalog_to_dataframe_has_event_type_column(self, example_catalog):
-        # test_cat = obspy.core.event.catalog._create_example_catalog()
         assert "event_type" in convert_catalog_to_dataframe(
             example_catalog).columns
 
     def test_convert_catalog_to_dataframe_has_origins_column(self, example_catalog):
-        # test_cat = obspy.core.event.catalog._create_example_catalog()
         assert "origins" in convert_catalog_to_dataframe(
             example_catalog).columns
