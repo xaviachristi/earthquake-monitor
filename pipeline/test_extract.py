f0a267b385c43259786ffeb52c10f36b4efdcc00
Em <trainee.emily.prust@sigmalabs.co.uk>
2025-06-11 14:14:20 +0100

    Add docstrings to tests

--- pipeline/test_extract.py
+++ pipeline/test_extract.py
@@ -8,17 +8,21 @@ import pandas as pd
 class TestConvertCatalogToDataFrame:
 
     def test_convert_catalog_to_dataframe_returns_dataframe(self, example_catalog):
+        """Checks the function returns a dataframe."""
         assert isinstance(convert_catalog_to_dataframe(
             example_catalog), pd.DataFrame)
 
     def test_convert_catalog_to_dataframe_has_magnitudes_column(self, example_catalog):
+        """Checks the function has a 'magnitudes' column."""
         assert "magnitudes" in convert_catalog_to_dataframe(
             example_catalog).columns
 
     def test_convert_catalog_to_dataframe_has_event_type_column(self, example_catalog):
+        """Checks the function has an 'event_type' column."""
         assert "event_type" in convert_catalog_to_dataframe(
             example_catalog).columns
 
     def test_convert_catalog_to_dataframe_has_origins_column(self, example_catalog):
+        """Checks the function has an 'origins' column."""
         assert "origins" in convert_catalog_to_dataframe(
             example_catalog).columns
