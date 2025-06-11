6507712fb8d985ed8d51055a0c75c7fd0284afe9
kcw36 <kylewat2000@hotmail.com>
2025-06-11 14:45:41 +0100

    Add subscribe page.

--- dashboard/subscription.py
+++ dashboard/subscription.py
@@ -9,5 +9,6 @@ def view_subscription():
     """Display information about subscriped topic."""
 
 
-def make_subscription():
+def make_subscription(first_name: str, last_name: str, email: str,
+                      latitude: float, longitude: float, radius: int, magnitude: float):
     """Create a subscription to a topic for their preference."""
