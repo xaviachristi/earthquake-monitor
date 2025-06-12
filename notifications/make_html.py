fb42de23ce3fe0eab0e0351d87904c9f8936c5c0
MariosT4 <micmartim@gmail.com>
2025-06-12 16:57:58 +0100

    Make script to check HTML output

---
+++ notifications/make_html.py
@@ -0,0 +1,18 @@
+"""Script to save a HTML so its format can be checked."""
+
+from datetime import datetime
+
+from notification_maker import make_message
+
+
+def write_to_html(message: str) -> None:
+    """Creates a html file with sample data."""
+    with open("fake_alert.html", "w", encoding="utf-8") as f:
+        f.write(message)
+
+
+if __name__ == "__main__":
+
+    fake_data = {"topic_arn": "a", "magnitude": 3.1, "state_name": "Not in the USA", "region_name": "Taiwan",
+                 "time": datetime.now(), "tsunami": False, "latitude": 30.101, "longitude": 50.123}
+    write_to_html(make_message(fake_data))
