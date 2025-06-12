040511f72a5380f2711050e5e2eb30f8a3b166ed
MariosT4 <micmartim@gmail.com>
2025-06-12 17:45:46 +0100

    Create lambda handler

--- notifications/lambda_handler.py
+++ notifications/lambda_handler.py
@@ -1,1 +1,13 @@
 """File that turns the notification-making into a lambda."""
+
+from notification_maker import send_emails
+
+
+def lambda_handler(event: dict, context: dict) -> dict:
+    """Creates a lambda handler."""
+    status = event.get("status_code")
+    if status == 200:
+        data = event.get("message")
+        send_emails(data)
+        return {None: None}
+    return {None: None}
