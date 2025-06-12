94712d9d800bd49d1e7c02a5e964854d06ff32c3
kcw36 <kylewat2000@hotmail.com>
2025-06-12 19:56:03 +0100

    Fix deploy for pipeline.

--- pipeline/docker_build.sh
+++ pipeline/docker_build.sh
@@ -1,4 +1,4 @@
 aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 129033205317.dkr.ecr.eu-west-2.amazonaws.com
-docker build --provenance=false --platform=linux/amd64  -t c17-kyle-trucks-report-ecr .
+docker build --provenance=false --platform=linux/amd64  -t c17-quake-pipeline-ecr:latest .
 docker tag c17-quake-pipeline-ecr:latest 129033205317.dkr.ecr.eu-west-2.amazonaws.com/c17-quake-pipeline-ecr:latest
 docker push 129033205317.dkr.ecr.eu-west-2.amazonaws.com/c17-quake-pipeline-ecr:latest
