2ae3a6dfd3aa807797ddb1bc5ac9ff8e0412a018
MariosT4 <micmartim@gmail.com>
2025-06-12 16:26:24 +0100

    Create test classes

--- notifications/test_notification_maker.py
+++ notifications/test_notification_maker.py
@@ -1,1 +1,25 @@
 """File that tests the notification-making functions."""
+
+
+from notification_maker import (validate_keys, validate_types,
+                                make_message, get_location_message)
+
+
+class TestValidateKeys:
+    """A class that groups together tests for validate_keys."""
+    ...
+
+
+class TestValidateTypes:
+    """A class that groups together tests for validate_types."""
+    ...
+
+
+class TestMakeMessage:
+    """A class that groups together tests for make_message."""
+    ...
+
+
+class TestGetLocationMessage:
+    """A class that groups together tests for get_location_message."""
+    ...
