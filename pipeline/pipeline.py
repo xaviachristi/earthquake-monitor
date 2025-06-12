627d5f69deb604c8861f597eb461c4e1b042d8cf
kcw36 <kylewat2000@hotmail.com>
2025-06-12 17:33:51 +0100

    Add some logs to pipeline.

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -5,7 +5,7 @@ from pytz import timezone
 from logging import getLogger, basicConfig
 from os import environ as ENV
 from math import radians, cos, sin, sqrt, atan2
-from re import fullmatch
+from re import search
 
 from pandas import DataFrame
 from dotenv import load_dotenv
@@ -32,7 +32,7 @@ def get_client() -> client:
 
 def get_haversine_distance(lat1, lon1, lat2, lon2):
     """
-    Calculate the great-circle distance between two points 
+    Calculate the great-circle distance between two points
     on the Earth (specified in decimal degrees).
     Returns distance in kilometers.
     """
@@ -87,12 +87,40 @@ def get_applicable_topics(topics: list[dict], row: dict) -> list[dict]:
     return subscribed_topics
 
 
-def get_topics(sns_client: client) -> list[str, str]:
+def get_topics(sns_client: client) -> list[dict]:
     """Return list of topic arns and names on SNS filtered for earthquake project."""
+    arns = []
+    topics = []
+    response = sns_client.list_topics()
+    next_token = response.get('NextToken', None)
+    arns = response.get('Topics')
+    logger.info("Found SNS Topics: %s", arns)
+    while next_token:
+        response = sns_client.list_topics(next_token)
+        next_token = response.get('NextToken', None)
+        arns.extend(response.get('Topics'))
+    for topic in arns:
+        topic_name = search(
+            r'c17-quake-\d{1,2}-(m|p)\d{1,2}-(m|p)\d{1,2}-\d+', topic.get('TopicArn'))
+        if topic_name:
+            topics.append(get_dict_from_topic(
+                topic_name.group(0), topic.get('TopicArn')))
+    logger.info("Found earthquake topics: %s", topics)
+    return topics
 
 
-def get_dicts_from_topics(topics: list[str, str]) -> list[dict]:
-    """Return list of dictionaries with seperated information from topics."""
+def get_dict_from_topic(name: str, arn: str) -> list[dict]:
+    """Return dictionary from topic with seperated information."""
+    parts = name.split("-")
+    lat = parts[3]
+    lon = parts[4]
+    return {
+        "topic_arn": arn,
+        "magnitude": float(parts[2]),
+        "latitude": float(lat[1:]) if lat[0] == "p" else float(-lat[1:]),
+        "longitude": float(lon[1:]) if lon[0] == "p" else float(-lon[1:]),
+        "radius": int(parts[5])
+    }
 
 
 def get_topic_dictionaries(data: DataFrame) -> list[dict]:
@@ -100,9 +128,10 @@ def get_topic_dictionaries(data: DataFrame) -> list[dict]:
     output_topics = []
     sns_client = get_client()
     topics = get_topics(sns_client)
-    topics_dicts = get_dicts_from_topics(topics)
+    logger.info("Extracted info from topics: %s", topics)
     for _, row in data.iterrows():
-        target_topics = get_applicable_topics(topics_dicts, row.to_dict())
+        target_topics = get_applicable_topics(topics, row.to_dict())
+        logger.info("Found subscriber topics: %s", target_topics)
         if target_topics:
             output_topics.extend(target_topics)
     return output_topics
@@ -206,4 +235,4 @@ if __name__ == "__main__":
         }
     ])
     load_dotenv()
-    print(load(sample_df))
+    logger.info("Topics: %s", get_topic_dictionaries(sample_df))
