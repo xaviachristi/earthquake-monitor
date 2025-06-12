91d4e2ff4bcf1cc645c090e7aab4a4ee713abe59
MariosT4 <micmartim@gmail.com>
2025-06-12 13:20:04 +0100

    Refactor validation, add send email function

--- notifications/notification_maker.py
+++ notifications/notification_maker.py
@@ -2,7 +2,9 @@
 
 import logging
 from datetime import datetime
+from os import environ as ENV
 
+from dotenv import load_dotenv
 from boto3 import client
 
 logger = logging.getLogger(__name__)
@@ -15,69 +17,31 @@ logging.basicConfig(
 
 
 def get_sns_client() -> client:
-    sns = client("sns")
+    sns = client("sns", aws_access_key_id=ENV["AWS_ACCESS_KEY"],
+                 aws_secret_access_key=ENV["AWS_SECRET_ACCESS_KEY"])
     return sns
 
 
-def validate_topic(data: dict) -> bool:
-    """Validates that the data contains a topic key."""
-    if data.get("topic") and len(data["topic"]) > 1:
-        return True
-    return False
+def validate_keys(data: dict) -> bool:
+    """Checks all the required keys are in the dictionary."""
+    keys = ["topic_arn", "magnitude", "state_name", "region_name", "time",
+            "tsunami", "latitude", "longitude"]
+    for key in keys:
+        if key not in data:
+            # TODO: Add log
+            return False
+    return True
 
 
-def validate_magnitude(data: dict) -> bool:
-    """Validates that the data contains a magnitude key."""
-    if data.get("magnitude") and isinstance(data["magnitude"], float):
-        return True
-    return False
-
-
-def validate_state(data: dict) -> bool:
-    """Validates that the data contains a state key."""
-    if data.get("state_name") and isinstance(data["state_name"], str):
-        return True
-    return False
-
-
-def validate_region(data: dict) -> bool:
-    """Validates that the data contains a region key."""
-    if data.get("region_name") and isinstance(data["region_name"], str):
-        return True
-    return False
-
-
-def validate_time(data: dict) -> bool:
-    """Validates that the data contains a time key."""
-    if data.get("time") and isinstance(data["time"], datetime):
-        return True
-    return False
-
-
-def validate_tsunami(data: dict) -> bool:
-    """Validates that the data contains a tsunami key."""
-    if data.get("tsunami") and isinstance(data["tsunami"], bool):
-        return True
-    return False
-
-
-def validate_latitude(data: dict) -> bool:
-    """Validates that the data contains a latitude key."""
-    if data.get("latitude") and isinstance(data["latitude"], float):
-        return True
-    return False
-
-
-def validate_longitude(data: dict) -> bool:
-    """Validates that the data contains a longitude key."""
-    if data.get("longitude") and isinstance(data["longitude"], str):
-        return True
-    return False
-
-
-def validate_data(data: dict) -> bool:
+def validate_types(data: dict) -> bool:
     """Validates that the required information for a message is available."""
-    ...
+    expected_data = [("topic_arn", str), ("magnitude", float), ("state_name", str), ("region_name", str),
+                     ("time", datetime), ("tsunami", bool), ("latitude", float), ("longitude", float)]
+    for name, expected_type in expected_data:
+        if not isinstance(data[name], expected_type):
+            # TODO: Add log
+            return False
+    return True
 
 
 def make_message(data: dict) -> str:
@@ -86,16 +50,14 @@ def make_message(data: dict) -> str:
     heading = """<!DOCTYPE html>
                  <html>
                  <body>
-                 <h1> Earthquake Alert! </h1>"""
-    body = f"""<p> There was an earthquake of magnitude {data["magnitude"]} in
-    the area of {data["state_name"]} in the region of {data["region_name"]} at {data["time"]}. </p>
+                 <h1>Earthquake Alert!</h1>"""
+    body = f"""<p>There was an earthquake of magnitude {data["magnitude"]} in
+    the area of {data["state_name"]} in the region of {data["region_name"]} at {data["time"]}.</p>
     """
-
     if data["tsunami"]:
-        body += """<p> There is potential for a tsunami. </p>"""
-
-    body += f"""<p> Precise latitude and longitude location of earthquake:
-      ({data["latitude"]}, {data["longitude"]}). </p>
+        body += """<p>There is potential for a tsunami.</p>"""
+    body += f"""<p>Precise latitude and longitude location of earthquake:
+      ({data["latitude"]}, {data["longitude"]}).</p>
       </body>
       </html>
       """
@@ -115,3 +77,17 @@ def publish_email(data: dict, sns: client) -> None:
                     message_id, topic)
     except sns.ClientError:
         logger.exception("Couldn't publish message to topic %s.", topic)
+
+
+def send_emails(passed_data: dict) -> None:
+    """Goes through all the topics generated."""
+    sns_client = get_sns_client()
+    for data in passed_data["topics"]:
+        if not validate_keys(data):
+            # TODO: Add logging
+            ...
+        elif not validate_types(data):
+            # TODO: Add logging
+            ...
+        else:
+            publish_email(data, sns_client)
