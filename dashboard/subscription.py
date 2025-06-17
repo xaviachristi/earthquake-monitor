98d84c060f607827eede08fe5bdfc8d91b5b0221
MariosT4 <micmartim@gmail.com>
2025-06-17 12:02:38 +0100

    Add logging

--- dashboard/subscription.py
+++ dashboard/subscription.py
@@ -3,7 +3,6 @@
 import logging
 from os import environ as ENV
 
-from dotenv import load_dotenv
 from boto3 import client
 
 logger = logging.getLogger(__name__)
@@ -14,11 +13,10 @@ logging.basicConfig(
     datefmt="%Y-%m-%dT%H:%M:%S"
 )
 
-# TODO: Add logging
-
 
 def get_sns_client() -> client:
     """Return a SNS client."""
+    logger.info("Creating SNS client...")
     sns = client("sns", aws_access_key_id=ENV["AWS_ACCESS_KEY"],
                  aws_secret_access_key=ENV["AWS_SECRET_ACCESS_KEY"])
     return sns
@@ -35,6 +33,7 @@ def view_subscription():
 def create_topic_name(topic_name: str, latitude: float, longitude: float,
                       radius: int, magnitude: float) -> str:
     """Creates a topic name based on the provided information."""
+    logger.info("Starting topic name creation...")
     if magnitude:
         magnitude = round(magnitude, 1)
         topic_name += f"-{magnitude}"
@@ -42,11 +41,13 @@ def create_topic_name(topic_name: str, latitude: float, longitude: float,
         latitude = location_formatting(latitude)
         longitude = location_formatting(longitude)
         topic_name += f"-{latitude}-{longitude}-{radius}"
+    logger.info("Topic name has been created.")
     return topic_name
 
 
 def location_formatting(coordinate: float) -> str:
     """Returns a formatted string of the latitude / longitude coordinate."""
+    logger.info("Formatting coordinate...")
     if coordinate < 0:
         return f"m{round(coordinate * -1, 4)}"
     return f"p{round(coordinate, 4)}"
@@ -54,11 +55,13 @@ def location_formatting(coordinate: float) -> str:
 
 def create_topic(sns: client, topic_name: str) -> str:
     """Creates a topic and returns the TopicARN."""
+    logger.info("Creating topic %s.", topic_name)
     return sns.create_topic(Name=topic_name)["TopicArn"]
 
 
 def sub_to_topic(sns: client, topic_arn: str, email: str) -> None:
     """Subscribes a user to a topic based on the TopicArn."""
+    logger.info("Subscribing %s to topic %s.", email, topic_arn)
     sns.subscribe(TopicArn=topic_arn,
                   Protocol="email",
                   Endpoint=email)
@@ -68,9 +71,11 @@ def make_subscription(email: str, latitude: float, longitude: float,
                       radius: int, magnitude: float) -> None:
     """Create a subscription to a topic for their preference.
     format: c17-quake-<magnitude>-(p/m)<latitude>-(p/m)<longitude>-<radius>"""
+    logger.info("Starting subscription creation...")
     topic_name = "c17-quake"
     topic_name = create_topic_name(
         topic_name, latitude, longitude, radius, magnitude)
     sns = get_sns_client()
     topic_arn = create_topic(sns, topic_name)
     sub_to_topic(sns, topic_arn, email)
+    logger.info("Subscription has been completed.")
