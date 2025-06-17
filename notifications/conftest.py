0da5bc54e89d75ef5be66524d68b1018f2e0b2d7
MariosT4 <micmartim@gmail.com>
2025-06-17 18:39:36 +0100

    Update time value

--- notifications/conftest.py
+++ notifications/conftest.py
@@ -9,7 +9,7 @@ from pytest import fixture
 def get_sample_data():
     """Expected data passed to notification maker."""
     data = {"topic_arn": "a", "magnitude": 3.1, "state_name": "state", "region_name": "region",
-            "time": datetime.now(), "tsunami": False, "latitude": 30.101, "longitude": 50.123}
+            "time": "2025/12/13 13:40", "tsunami": False, "latitude": 30.101, "longitude": 50.123}
     return data
 
 
@@ -18,5 +18,5 @@ def get_sample_non_usa_data():
     """Expected data passed to notification maker
     when an earthquake outside of the USA is detected."""
     data = {"topic_arn": "a", "magnitude": 3.1, "state_name": "Not in the USA", "region_name": "Taiwan",
-            "time": datetime.now(), "tsunami": False, "latitude": 30.101, "longitude": 50.123}
+            "time": "2025/12/13 13:40", "tsunami": False, "latitude": 30.101, "longitude": 50.123}
     return data
