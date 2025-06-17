18d0c81394600b1c2f21f7ebaa0dda61151fcf07
kcw36 <kylewat2000@hotmail.com>
2025-06-17 17:32:59 +0100

    Add time window to pipeline and pipeline lambda.

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,5 +1,6 @@
 """Script for running full ETL pipeline as AWS lambda."""
 
+from argparse import ArgumentParser, Namespace
 from datetime import datetime, timedelta
 from logging import getLogger, basicConfig
 from os import environ as ENV
@@ -62,10 +63,11 @@ def lambda_handler(event, context):
     """
     try:
         logger.info("Getting time window from event...")
+        start_time, end_time = get_time_window_from_cli(event)
 
         logger.info("Running ETL pipeline...")
-        data = run_pipeline(datetime.now() - timedelta(hours=12),
-                            datetime.now())
+        data = run_pipeline(start_time,
+                            end_time)
 
         topics = None
 
@@ -84,7 +86,52 @@ def lambda_handler(event, context):
         raise RuntimeError(f"Python pipeline failed to execute: {e}") from e
 
 
+def get_time_window_from_cli() -> tuple[datetime, datetime]:
+    """Get start and end time from runtime arguments for local pipeline."""
+    parser = ArgumentParser(description="Process start and end times.")
+
+    parser.add_argument(
+        '--start',
+        required=True,
+        help="Start time in format YYYY-MM-DD HH:MM"
+    )
+
+    parser.add_argument(
+        '--end',
+        '-e',
+        required=False,
+        help="End time in format YYYY-MM-DD HH:MM"
+    )
+
+    args = parser.parse_args()
+    return get_datetimes(args.start, args.end)
+
+
+def get_time_window_from_event(event) -> tuple[datetime, datetime]:
+    """
+    Get start and end time, in format "%Y-%m-%d %H:%M",
+    from runtime event for lambda pipeline.
+    """
+    start_time = event.get("start")
+    end_time = event.get("end", None)
+    return get_datetimes(start_time, end_time)
+
+
+def get_datetimes(start: str, end: str = None):
+    """Return datetime tuple from strings."""
+    logger.info("Getting datetime from strings...")
+    start_time = datetime.strptime(start, "%Y-%m-%d %H:%M")
+    if end:
+        end_time = datetime.strptime(end, "%Y-%m-%d %H:%M")
+    else:
+        end_time = datetime.now()
+
+    if start_time >= end_time:
+        logger.error("Error: Start time must be before end time.")
+    return (start_time, end_time)
+
+
 if __name__ == "__main__":
     load_dotenv()
-    run_pipeline(datetime.now() - timedelta(hours=12),
-                 datetime.now())
+    window_start, window_end = get_time_window_from_cli()
+    run_pipeline(window_start, window_end)
