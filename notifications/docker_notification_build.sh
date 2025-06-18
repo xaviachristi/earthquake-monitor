09614ec3d265184665dbb84faee2918f649b9c46
MariosT4 <micmartim@gmail.com>
2025-06-18 17:43:06 +0100

    Update upload script to tf ecr

--- notifications/docker_notification_build.sh
+++ notifications/docker_notification_build.sh
@@ -1,4 +1,4 @@
 aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 129033205317.dkr.ecr.eu-west-2.amazonaws.com
-docker build -t c17-quake-notification-ecr --provenance=false --platform=linux/amd64 .
-docker tag c17-quake-notification-ecr:latest 129033205317.dkr.ecr.eu-west-2.amazonaws.com/c17-quake-notification-ecr:latest
-docker push 129033205317.dkr.ecr.eu-west-2.amazonaws.com/c17-quake-notification-ecr:latest
+docker build -t c17-quake-notification-ecr-tf --provenance=false --platform=linux/amd64 .
+docker tag c17-quake-notification-ecr-tf:latest 129033205317.dkr.ecr.eu-west-2.amazonaws.com/c17-quake-notification-ecr-tf:latest
+docker push 129033205317.dkr.ecr.eu-west-2.amazonaws.com/c17-quake-notification-ecr-tf:latest
