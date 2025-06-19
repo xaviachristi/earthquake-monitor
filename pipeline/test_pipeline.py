afa4b5d9a8182c5d6f16c4833bf3b0fb8414616b
kcw36 <kylewat2000@hotmail.com>
2025-06-19 12:14:11 +0100

    Initiate pipeline tests.

--- pipeline/test_pipeline.py
+++ pipeline/test_pipeline.py
@@ -1,12 +1,4 @@
 # pylint: skip-file
 """Unit tests for the functions in pipeline.py."""
 
-from pipeline import get_topic_dictionaries
-
-
-class TestGetTopicDicitionaries:
-    """A class that groups together tests for get_topic_dictionaries."""
-
-    def test_get_topic_dictionaries_returns_list(self, example_diff, example_topic):
-        """Check that get_topic_dictionaries returns expected list of topics."""
-        assert True
+from pipeline import run_pipeline,
